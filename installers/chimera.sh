96f6e22a1449363e67907825f9aa4a3869a57b03
Marko Djukic <marko@ingenii.dev>
2022-06-29 16:19:20 +0000

    fix: adding chimera as a package to the Docker

---
+++ installers/chimera.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+##
+# Chimera requires a license acceptance to allow downloads, and the installer needs user interaction.
+# This script automates some of this to help package into the Docker image.
+#
+##
+
+# Do all of this in the tmp folder.
+tmp_dir=/tmp/$$_chimera
+echo "Creating temp directory ${tmp_dir}"
+mkdir ${tmp_dir}
+cd ${tmp_dir}
+
+CHIMERA_LINK="https://www.cgl.ucsf.edu$(curl -X POST -d "choice=Accept&file=linux_x86_64_osmesa%2Fchimera-1.16-linux_x86_64_osmesa.bin" -X POST https://www.cgl.ucsf.edu/chimera/cgi-bin/secure/chimera-get.py | grep "href=\"/chimera" | sed 's/.*"\(.*\)".*/\1/')"
+
+echo $CHIMERA_LINK
+
+curl $CHIMERA_LINK --output chimera_installer.bin
+
+unzip chimera_installer.bin
+./chimera.bin -q -d /opt/chimera
+ln -s /opt/chimera/bin/chimera /usr/local/bin
+
+cd /
+rm -rf ${tmp_dir}
+ 
