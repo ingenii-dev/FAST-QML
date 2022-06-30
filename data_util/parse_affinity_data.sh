40c0f8ab3baaf676cc41d0680106489ca4a65a3c
laiadc <laia.d.c@hotmail.com>
2022-06-30 20:58:59 +0200

    fixes

--- data_util/parse_affinity_data.sh
+++ data_util/parse_affinity_data.sh
@@ -8,12 +8,10 @@
 # based on implementation provide in https://gitlab.com/cheminfIBB/pafnucy/blob/master/pdbbind_data.ipynb
 ################################################################################
 
-
-# user specify path as first arg
 path=$1
 
 echo 'pdbid,-logKd/Ki' > affinity_data.csv
-cat $path/PDBbind_2016_plain_text_index/index/INDEX_general_PL_data.2016 | while read l1 l2 l3 l4 l5; do
+cat $path/general-set-except-refined/index/INDEX_general_PL_data.2016 | while read l1 l2 l3 l4 l5; do
     if [[ ! $l1 =~ "#" ]]; then
         echo $l1,$l4
     fi
@@ -23,13 +21,15 @@ done >> affinity_data.csv
 # Find affinities without structural data (i.e. with missing directories)
 
 cut -f 1 -d ',' affinity_data.csv | tail -n +2 | while read l;
-    do if [ ! -e $path/general-set-except-refined/$l ] &&  [ ! -e $path/refined-set/$l ]; then
+    do if [ ! -e $path/general-set-except-refined/$l ] && [ ! -e $path/refined-set/$l ]; then 
         echo $l;
     fi
 done
 
 
 
-grep -v '#' $path/PDBbind_2016_plain_text_index/index/INDEX_core_data.2016 | cut -f 1 -d ' '  > core.csv
+grep -v '#' $path/refined-set/index/INDEX_refined_data.2016 | cut -f 1 -d ' ' > refined.csv
 
-grep -v '#' $path/PDBbind_2016_plain_text_index/index/INDEX_refined_data.2016 | cut -f 1 -d ' ' > refined.csv
+grep -v '#' $path/refined-set/index/INDEX_core_data.2016 | cut -f 1 -d ' '  > core.csv
+
+
